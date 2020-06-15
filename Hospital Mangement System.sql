4f64dae4ce2bced9439b662c085ca3ab91d08f7e
Ahmed Esam <66920624+ahmedesam66@users.noreply.github.com>
2020-06-15 17:33:56 +0200

    Add files via upload

---
+++ Hospital Mangement System.sql
@@ -0,0 +1,7 @@
+use `Hospital_Mangement_System`;
+SELECT * FROM hospital_mangement_system.doctor;
+SELECT * FROM hospital_mangement_system.patient;
+SELECT * FROM hospital_mangement_system.bill;
+SELECT * FROM hospital_mangement_system.room;
+SELECT * FROM hospital_mangement_system.inpatient;
+SELECT * FROM hospital_mangement_system.lab;
